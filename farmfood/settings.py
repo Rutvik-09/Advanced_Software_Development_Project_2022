f087836a81b362f7c9fe7b9b44ad4e89c6ce8f5b
Vinay Patil <vn537971@dal.ca>
2022-02-08 19:56:15 -0400

    added urls for app

--- farmfood/settings.py
+++ farmfood/settings.py
@@ -25,12 +25,15 @@ SECRET_KEY = 'django-insecure-re=rnbs!oy3_7mpbbp4+y&48yvlmfpn)5ouc@h4h3naswk6755
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    # User Apps
+    'farmfoodapp',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

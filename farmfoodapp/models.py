07acd7f57eef64776b4c41b9099ac5b1a88dd644
Vinay Patil <vn537971@dal.ca>
2022-02-13 19:23:07 -0400

    added models to project

--- farmfoodapp/models.py
+++ farmfoodapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class RegisterModel(models.Model):
+    first_name = models.CharField(max_length=50)  # Required Field
+    last_name = models.CharField(max_length=50)  # Required Field
+    date_of_birth = models.DateTimeField()  # Required Field
+    username = models.CharField(max_length=50, default="")  # Optional Field
+    email = models.EmailField(max_length=50)  # Required Field
+    country_code = models.CharField(max_length=5, default="+1")  # Optional Field
+    phone = models.CharField(max_length=15)  # Required Field
+    user_password = models.CharField(max_length=50)
--- farmfoodapp/views.py
+++ farmfoodapp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from farmfoodapp.models import *
 
 # Create your views here.
+from rest_framework.decorators import api_view
+
+
+@api_view(["POST"])
+def register_api(request):
+    user_data = request.data
+    # To Parse Data
+    # register_obj = RegisterModel(user_data)
+    # register_obj.save()
+    return True
+
+
+@api_view(["POST"])
+def login_api(request):
+    user_data = request.data
+    return True
+
+
+@api_view(["POST"])
+def reset_secret(request):
+    user_data = request.data
+    return True

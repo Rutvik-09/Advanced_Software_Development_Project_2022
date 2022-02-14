0dc4aa3fbeee37bfd65ed4ce29aa4910e869fc7e
Vinay Patil <vn537971@dal.ca>
2022-02-14 12:53:36 -0400

    testing code quality

--- farmfoodapp/views.py
+++ farmfoodapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from farmfoodapp.models import *
+# from farmfoodapp.models import *
 
 # Create your views here.
 from rest_framework.decorators import api_view
@@ -7,7 +7,7 @@ from rest_framework.decorators import api_view
 
 @api_view(["POST"])
 def register_api(request):
-    user_data = request.data
+    # user_data = request.data
     # To Parse Data
     # register_obj = RegisterModel(user_data)
     # register_obj.save()
@@ -16,13 +16,13 @@ def register_api(request):
 
 @api_view(["POST"])
 def login_api(request):
-    user_data = request.data
+    # user_data = request.data
     return True
 
 
 @api_view(["POST"])
 def reset_secret(request):
-    user_data = request.data
+    # user_data = request.data
     return True
 
 
